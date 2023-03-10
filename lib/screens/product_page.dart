460b73966d60f4ecd1152e38fe463b5c26b1f5d4
Mayank0010 <kr.mayank.singh@gmail.com>
2023-03-10 14:26:32 +0530

    done

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,116 +1,92 @@
 import 'package:flutter/material.dart';
 
-class Product {
-  final String name;
-  final double price;
-  final String image;
-  final String description;
-
-  Product(this.name, this.price, this.image, this.description);
-}
-
-class ProductsPage extends StatefulWidget {
-  @override
-  _ProductsPageState createState() => _ProductsPageState();
-}
-
-class _ProductsPageState extends State<ProductsPage> {
-  List<Product> _products = [];
-
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _priceController = TextEditingController();
-  TextEditingController _imageController = TextEditingController();
-  TextEditingController _descriptionController = TextEditingController();
-
+class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Products"),
+        title: Text('Products'),
       ),
-      body: ListView.builder(
-        itemCount: _products.length,
-        itemBuilder: (context, index) {
-          Product product = _products[index];
-          return ListTile(
-            leading: Image.network(product.image),
-            title: Text(product.name),
-            subtitle: Text("\$${product.price}\n${product.description}"),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                setState(() {
-                  _products.removeAt(index);
-                });
-              },
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Add a new product',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  SizedBox(height: 26.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Product Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a product name';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 26.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Price',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter product price';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 26.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Image URL',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a product image';
+                      }
+                      return null;
+                    },
+                  ),
+                 SizedBox(height: 26.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a product description';
+                      }
+                      return null;
+                    },
+                    maxLines: 2,
+                  ),
+                  SizedBox(height: 26.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add product logic here
+                    },
+                    child: Text('Add Product', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _showAddProductDialog(context);
-        },
-      ),
-    );
-  }
-
-  void _showAddProductDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Add Product"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: "Name"),
-              ),
-              TextField(
-                controller: _priceController,
-                decoration: InputDecoration(labelText: "Price"),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _imageController,
-                decoration: InputDecoration(labelText: "Image URL"),
-              ),
-              TextField(
-                controller: _descriptionController,
-                decoration: InputDecoration(labelText: "Description"),
-                maxLines: 3,
-              ),
-            ],
           ),
-          actions: [
-            TextButton(
-              child: Text("Cancel"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("Add"),
-              onPressed: () {
-                String name = _nameController.text;
-                double price = double.tryParse(_priceController.text) ?? 0.0;
-                String image = _imageController.text;
-                String description = _descriptionController.text;
-
-                if (name.isNotEmpty && price > 0.0 && image.isNotEmpty && description.isNotEmpty) {
-                  setState(() {
-                    _products.add(Product(name, price, image, description));
-                  });
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
-        );
-      },
+        ),
+      ),
     );
   }
 }
--- lib/screens/worker_verification.dart
+++ lib/screens/worker_verification.dart
@@ -1,198 +1,57 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-class WorkerVerificationScreen extends StatefulWidget {
-  @override
-  _WorkerVerificationScreenState createState() =>
-      _WorkerVerificationScreenState();
-}
-
-class _WorkerVerificationScreenState extends State<WorkerVerificationScreen> {
-  File _document;
-  File _image;
-  File _video;
-  String _interviewDate;
-  bool _isSubmitting = false;
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  Future<void> _pickDocument() async {
-    final pickedFile = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-      imageQuality: 50,
-    );
-
-    if (pickedFile != null) {
-      setState(() {
-        _document = File(pickedFile.path);
-      });
-    }
-  }
-
-  Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-      imageQuality: 50,
-    );
-
-    if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
-    }
-  }
-
-  Future<void> _pickVideo() async {
-    final pickedFile = await ImagePicker().pickVideo(
-      source: ImageSource.gallery,
-      maxDuration: Duration(minutes: 5),
-    );
-
-    if (pickedFile != null) {
-      setState(() {
-        _video = File(pickedFile.path);
-      });
-    }
-  }
-
-  Future<void> _scheduleInterview() async {
-    // Call an API to schedule an interview with the admin
-    // You can use a date picker widget or a text input field to get the interview date
-    // Once the interview is scheduled, show a confirmation message to the user
-  }
-
-  Future<void> _submitForm() async {
-    if (!_formKey.currentState.validate()) {
-      return;
-    }
-
-    setState(() {
-      _isSubmitting = true;
-    });
-
-    // Call an API to submit the form data and the uploaded files
-    // Once the submission is completed, show a success message to the user
-    // You may also want to navigate to a different screen or pop the current screen from the stack
-  }
-
+class WorkerVerificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('Worker Verification'),
-    ),
-    body: Form(
-    key: _formKey,
-    child: GridView.count(
-    crossAxisCount: 2,
-    padding: EdgeInsets.all(16),
-    childAspectRatio: 1,
-    mainAxisSpacing: 16,
-    crossAxisSpacing: 16,
-    children: [
-    GestureDetector(
-    onTap: _pickDocument,
-    child: Container(
-    decoration: BoxDecoration(
-    border: Border.all(
-    color: Colors.grey[300],
-    width: 1,
-    ),
-    ),
-    child: _document == null
-    ? Icon(Icons.picture_as_pdf,
-    size: 72, color: Colors.grey)
-        : Image.file(_document, fit: BoxFit.cover),
-    ),
-    ),
-    GestureDetector(
-    onTap: _pickImage,
-    child: Container(
-    decoration: BoxDecoration(
-    border: Border.all(
-    color: Colors.grey[300],
-    width: 1,
-    ),
-      child: _image == null
-          ? Icon(Icons.image, size: 72, color: Colors.grey)
-          : Image.file(_image, fit: BoxFit.cover),
-    ),
-    ),
-      GestureDetector(
-        onTap: _pickVideo,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey[300],
-              width: 1,
-            ),
-          ),
-          child: _video == null
-              ? Icon(Icons.videocam, size: 72, color: Colors.grey)
-              : AspectRatio(
-            aspectRatio: 1 / 1,
-            child: VideoPlayerWidget(file: _video),
-          ),
-        ),
       ),
-      GestureDetector(
-        onTap: _scheduleInterview,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey[300],
-              width: 1,
+      body: Column(
+        children: <Widget>[
+          // List of uploaded documents, images, and videos
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.description),
+                  title: Text('Resume'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // TODO: Open resume document
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo),
+                  title: Text('Photo ID'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // TODO: Open photo ID image
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.video_collection),
+                  title: Text('Introduction Video'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // TODO: Play introduction video
+                  },
+                ),
+              ],
             ),
           ),
-          child: Center(
-            child: Text(
-              'Schedule Interview',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-              ),
+          // Schedule interview button
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Schedule interview
+              },
+              child: Text('Schedule Interview', style: TextStyle(color: Colors.white)),
             ),
           ),
-        ),
-      ),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      Container(),
-      ],
-    ),
-    ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _isSubmitting ? null : _submitForm,
-        icon: _isSubmitting
-            ? SizedBox(
-          width: 24,
-          height: 24,
-          child: CircularProgressIndicator(
-            strokeWidth: 3,
-            valueColor: AlwaysStoppedAnimation(Colors.white),
-          ),
-        )
-            : Icon(Icons.check),
-        label: Text('Submit'),
+
+        ],
       ),
     );
   }
