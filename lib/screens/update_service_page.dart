7856fd378e6c13adee8c8cc0b224ce85cdc28349
Mayank0010 <kr.mayank.singh@gmail.com>
2023-03-18 11:55:56 +0530

    Implemented the CRUD operation for products and services.

---
+++ lib/screens/update_service_page.dart
@@ -0,0 +1,134 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class UpdateServicePage extends StatefulWidget {
+  final String name;
+  final String description;
+  final String price;
+  final String image;
+  final DocumentReference reference;
+
+  UpdateServicePage({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.reference,
+  });
+
+  @override
+  _UpdateServicePageState createState() => _UpdateServicePageState();
+}
+
+class _UpdateServicePageState extends State<UpdateServicePage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+  late TextEditingController _descriptionController;
+  late TextEditingController _priceController;
+  late TextEditingController _imageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.name);
+    _descriptionController = TextEditingController(text: widget.description);
+    _priceController = TextEditingController(text: widget.price);
+    _imageController = TextEditingController(text: widget.image);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _priceController,
+                decoration: InputDecoration(
+                  labelText: 'Price',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a price';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _imageController,
+                decoration: InputDecoration(
+                  labelText: 'Image URL',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter an image URL';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    await widget.reference.update({
+                      'name': _nameController.text,
+                      'description': _descriptionController.text,
+                      'price': _priceController.text,
+                      'image': _imageController.text,
+                    });
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Update Product'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    _priceController.dispose();
+    _imageController.dispose();
+    super.dispose();
+  }
+}
